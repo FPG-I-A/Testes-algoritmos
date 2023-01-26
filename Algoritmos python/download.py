2a021dbcfe167b97a7568cbe21d7c5f3f61dbb50
Luís Otávio Amorim <luis.amorim@etapa.com.br>
2023-01-26 19:45:16 -0300

    fix: Dataset só é baixado quando não existe  Changes to be committed: 	modified:   download.py

--- Algoritmos python/download.py
+++ Algoritmos python/download.py
@@ -5,5 +5,8 @@ import requests
 response = requests.get(
     'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
 )
+
+if not os.path.exists('Dados'):
+    os.mkdir('Dados')
 with open(os.path.join('Dados', 'iris.csv'), mode='wb') as f:
     f.write(response.content)
